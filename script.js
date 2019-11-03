5219b184a06bbff060f4b033a8db138672f757c6
Jeremy Corren <jeremycorrenmusic@gmail.com>
2019-11-03 01:28:22 -0400

    Responsive font

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ $(document).ready(function() {
   $("#about-content").show();
 
   $("#about-link").click(function() {
-    $("#about-content").fadeIn();
+    $("#about-content").show();
     $("#music-content").hide();
     $("#contact-content").hide();
   });
 
   $("#music-link").click(function() {
     $("#about-content").hide();
-    $("#music-content").fadeIn();
+    $("#music-content").show();
     $("#contact-content").hide();
   });
 
   $("#contact-link").click(function() {
     $("#about-content").hide();
     $("#music-content").hide();
-    $("#contact-content").fadeIn();
+    $("#contact-content").show();
   });
 });
